e410b9cf0d3a5f9d18a3c29475d3cbaea2862054
junidy <junidyeom@gmail.com>
2024-04-12 18:33:20 -0400

    Login/register hooked up

--- frontend/src/components/RegisterTitle/RegisterTitle.jsx
+++ frontend/src/components/RegisterTitle/RegisterTitle.jsx
@@ -7,12 +7,47 @@ import {
     Text,
     Container,
     Button,
+    Select,
+    SegmentedControl,
   } from '@mantine/core';
 import { PasswordStrength } from "../PasswordStrength.jsx";
 import classes from './RegisterTitle.module.css';
 import { DropdownMenu } from '../DropdownMenu/DropdownMenu.jsx';
+import { postRegister } from '../../utils/endpoints.js';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const data = [
+  { label: 'University of Central Florida', value: '1'},
+  { label: 'Harvard University', value: '2'},
+];
   
   export function RegisterTitle() {
+    const nav = useNavigate();
+    const [creds, setCreds] = useState({
+        password: "",
+        name: "",
+        phone: "", 
+        email: "", 
+        university_id: null,
+        isAdmin: false
+    });
+    const handleChange = event => {
+        const target = event.target.getAttribute('name');
+        setCreds({
+            ...creds,
+            [target]: event.target.value,
+        })
+    }
+    const handleRegister = e => {
+        postRegister(creds)
+            .then(token => {
+                console.log(token);
+                sessionStorage.setItem('token', token);
+                nav('../events');
+            })
+            .catch(error => console.log(error));
+    }
     return (
         <div>
             <Container size={500} my={40}>
@@ -27,13 +62,16 @@ import { DropdownMenu } from '../DropdownMenu/DropdownMenu.jsx';
                 </Text>
         
                 <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-                <TextInput label="Name" placeholder="Name" required ></TextInput>
-                <TextInput label="Email" placeholder="you@mantine.dev" required />
+                <TextInput value={creds.name} onChange={handleChange} label="Name" placeholder="Name" name="name" required ></TextInput>
+                <TextInput value={creds.email} onChange={handleChange} label="Email" placeholder="you@mantine.dev" name="email" required />
+                <TextInput value={creds.phone} onChange={handleChange} label="Phone" placeholder="555-555-5555" name="phone" required />
                 <PasswordStrength></PasswordStrength>
-                <PasswordInput label="Confirm Password" placeholder="Confirm password" required mt="md" />
-                <Text>Select your university:</Text>
-                <DropdownMenu></DropdownMenu>
-                <Button fullWidth mt="xl">
+                <PasswordInput value={creds.password} onChange={handleChange} label="Confirm Password" placeholder="Confirm password" name="password" required mt="md" />
+                {/* <Text>Select your university:</Text> */}
+                {/* <DropdownMenu onChange={handleChange}></DropdownMenu> */}
+                <Select value={creds.university} onChange={(v, o) => setCreds({...creds, university_id: v})} label="University" placeholder="Select your university" data={data} required />
+                <Select onChange={(v, o) => setCreds({...creds, isAdmin: v === "RSO Administrator"})} label="Account type" defaultValue="Student" data={['Student', 'RSO Administrator']} required />
+                <Button onClick={handleRegister} fullWidth mt="xl">
                     Create account
                 </Button>
                 </Paper>
