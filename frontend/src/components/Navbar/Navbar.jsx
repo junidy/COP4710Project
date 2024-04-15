66dbdbb10dc3b1561ada60f82be7e98df7deaf3d
John Boyd <46943648+johnb254@users.noreply.github.com>
2024-04-15 02:33:56 -0400

    Update Navbar.jsx
    Navbar now allows you to sign out if signed in. Signing out will bring you to the homepage.

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
 import { Group, Button, Text, Box, Burger, Drawer, ScrollArea, Center } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
-import { Link } from 'react-router-dom';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
 import classes from './Navbar.module.css';
 
 export function Navbar() {
   const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] = useDisclosure(false);
+  const nav = useNavigate();
 
   return (
     <Box pb={120}>
@@ -17,18 +17,49 @@ export function Navbar() {
             <a href="/" className={classes.link}>
               Home
             </a>
-            <a href="/events" className={classes.link}>
-              Events
-            </a>
-            <a href="/clubs" className={classes.link}>
-              Clubs
-            </a>
+            {/* redirect to login page if not logged in */}
+            {sessionStorage.getItem('token') ? (
+              <>
+                <a href="/events" className={classes.link}>
+                  Events
+                </a>
+                <a href="/clubs" className={classes.link}>
+                  Clubs
+                </a>
+              </>
+            ) : (
+              <>
+                <a href="/login" className={classes.link}>
+                  Events
+                </a>
+                <a href="/login" className={classes.link}>
+                  Clubs
+                </a>
+              </>
+            )}
+
+
           </Center>
         </Group>
 
         <Group style={{ flex: 1, justifyContent: 'flex-end' }} visibleFrom='sm'>
-          <Button variant="default" component={Link} to="/login">Log in</Button>
-          <Button component={Link} to="/register">Sign up</Button>
+          {/* if user is logged in, show sign out button, else show login and register buttons */
+            // if jwt token is present, show sign out button
+            sessionStorage.getItem('token') ? (
+              <Button
+                onClick={() => {
+                  sessionStorage.removeItem('token');
+                  nav('/');
+                }}
+              >
+                Sign out
+              </Button>
+            ) : (
+              <>
+                <Button variant="default" component={Link} to="/login">Log in</Button>
+                <Button component={Link} to="/register">Sign up</Button>
+              </>
+            )}
         </Group>
 
         <Burger opened={drawerOpened} onClick={toggleDrawer} hiddenFrom="sm" />
